e2d0744ad6caceaa08c1584b94842c0f48e7f5f4
hasnainzxc <hasnainzxc@icloud.com>
2023-11-07 16:27:58 +0500

    working wallet connect

---
+++ .history/src/App_20231107161406.tsx
@@ -0,0 +1,29 @@
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { arbitrum, mainnet } from "viem/chains";
+
+// 1. Get projectId
+const projectId = "744f73888bf542c84f49e578463ddaea";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+export default function App() {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <w3m-button />
+    </WagmiConfig>
+  );
+}
---
+++ .history/src/App_20231107162349.tsx
@@ -0,0 +1,44 @@
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { arbitrum, mainnet } from "viem/chains";
+
+// 1. Get projectId
+const projectId = "744f73888bf542c84f49e578463ddaea";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum];
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({
+      chains,
+      options: { projectId, showQrModal: false, metadata },
+    }),
+    new EIP6963Connector({ chains }),
+    new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: { appName: metadata.name },
+    }),
+  ],
+  publicClient,
+});
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+export default function App() {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <w3m-button />
+    </WagmiConfig>
+  );
+}
